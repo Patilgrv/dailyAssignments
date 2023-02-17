271de974c5ee7c1c8bb0e9dc1d1ca87e6ca4032a
Patilgrv <patilgaurav189@gmail.com>
2023-02-17 13:18:43 +0530

    file created for basic variable types
    created files which describes primitive data type- number, string and Boolean and print result along with its type

---
+++ 01_vaiableBasicsAssign.js
@@ -0,0 +1,14 @@
+var birthYear = 1995
+var myName = "Gaurav"
+var isSingle = true
+
+console.log('Year of birth: ',birthYear,'type:',typeof birthYear);
+console.log('My Name: ',myName,'type:',typeof myName);
+console.log('Relationship Status is single:',isSingle,'type:',typeof isSingle);
+
+
+var newNull = null
+var iAmUndefined;
+
+console.log('i am',newNull, 'and my type is', typeof newNull);
+console.log('I am',iAmUndefined,' and my type is',typeof iAmUndefined);
---
+++ 01_variable_assign.js
@@ -0,0 +1,22 @@
+console.log(`*******Step1*******`);
+console.log(myFullName);
+var myFullName =  `Gaurav Patil`
+console.log('My Name is :', myFullName);
+myFullName = `Gaurav Rajendra Patil`
+console.log('My Full Name is :', myFullName);
+
+
+console.log(`*******Step2*******`);
+var myLove = (`I love only JS`)
+console.log(myLove);
+
+console.log(`*******Step3*******`);
+var firstName = `Gaurav`
+var lastName = `Patil`
+var age = `27`
+var address = `Indore`
+var pincode = `452010`
+var gender = 'Male'
+var isMarried = `Single`
+
+console.log('My Name is',firstName + ' ' + lastName, 'my age is', age,'I live at',address +','+ pincode,'. My gender is:',gender,'and marital status is:',isMarried);
