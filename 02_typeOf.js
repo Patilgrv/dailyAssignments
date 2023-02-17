69b0d4112a216cace8e085ae7b4898036215d042
Patilgrv <patilgaurav189@gmail.com>
2023-02-17 13:30:39 +0530

    Create 02_typeOf.js
    variable, dataType and type of Operator

---
+++ 02_typeOf.js
@@ -0,0 +1,19 @@
+// _______1_______
+var bankName 
+console.log(bankName);
+console.log(typeof bankName);
+bankName = 'SBI Bank'
+console.log(bankName);
+console.log('type of var bankName is:',typeof bankName);
+// ________2__________
+var marks = '90%'
+console.log('marks:', marks, 'type of marks is', typeof marks);
+// __________3__________
+var isWorking;
+console.log(isWorking);
+isWorking = true;
+console.log(isWorking);
+console.log('type of var isWorking is:', typeof isWorking);
+// __________4____________
+var totalCount = "Hundred and Seven"
+console.log('totalCount is:',totalCount,'and its data type is:', typeof totalCount);
