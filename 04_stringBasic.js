a2c281ef95f3e48074338cce959beafb734b76ec
Patilgrv <patilgaurav189@gmail.com>
2023-02-22 12:48:47 +0530

    Create 04_stringBasic.js
    string Assignment 1 done

---
+++ 04_stringBasic.js
@@ -0,0 +1,29 @@
+console.log("Assignment1");
+
+function stringBasic(){
+    console.log(`My dream company is having my own company`);
+}
+stringBasic()
+
+let hobby1 = "tracking" 
+let hobby2 = "cleaning" 
+let hobby3 = "sleeping" 
+console.log(`My hobbies are ${hobby1}, ${hobby2} and ${hobby3}`);
+
+function charCount(hobby1,hobby2,hobby3){
+
+charCountHobby1 = hobby1.length;
+charCountHobby2 = hobby2.length;
+charCountHobby3 = hobby3.length;
+
+totalHobbylength = charCountHobby1 + charCountHobby2 + charCountHobby3;
+    return totalHobbylength
+}
+
+hobby1 = "tracking" 
+hobby2 = "cleaning" 
+hobby3 = "sleeping" 
+console.log( `Number of character in all three hobbies are`,charCount(hobby1,hobby2,hobby3));
+
+
+console.log (`___________AssignmentB___________`)
