8a1f90f7209b7c885d8af9b4cc54fe88ade2a2a5
Patilgrv <patilgaurav189@gmail.com>
2023-02-28 21:24:32 +0530

    Update 08_switch_leapYear.js
    correction done to leap year assignment to handle bad input value

--- 08_switch_leapYear.js
+++ 08_switch_leapYear.js
@@ -2,8 +2,10 @@
 
 let = checkLeapYear = function(year)
 {
-  if (((year % 4 == 0) && (year % 100 != 0)) || (year % 400 == 0) && (year != null
-    ))
+  if(year==null|| year == "string"|| year == undefined|| isNaN(year)==true)
+  {console.log(`${year} :Invalid data, Please enter correct data`);}
+
+  else if (((year % 4 == 0) && (year % 100 != 0)) || (year % 400 == 0) )
    {console.log(`Given year ${year} is a leap year`);} 
 
 else {console.log(`${year}:Not a leap year`);}
