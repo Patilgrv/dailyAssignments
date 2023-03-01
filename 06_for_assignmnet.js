adf9d47685b8ae654b9807772b1470f34403d0d6
Patilgrv <patilgaurav189@gmail.com>
2023-03-01 19:34:58 +0530

    Create 06_for_assignmnet.js
    for loop assignment

---
+++ 06_for_assignmnet.js
@@ -0,0 +1,56 @@
+console.log("________________1______________");
+console.log("WAP to print numbers from 5 to 15 by incrementing 1");
+
+for (var index = 5; index <= 15; index= index+1) {
+    console.log(index);; 
+}
+
+
+ console.log("________________2______________");
+ console.log("WAP to print numbers from 50 to 40 by decrementing 1");
+
+for (var index = 50; index >= 40; index= index-1) {
+    console.log(index);
+}
+
+
+
+console.log("________________3______________");
+console.log("WAP to find first 15 odd numbers");
+
+for (var index = 1; index <=30; index= index+2) {
+   console.log(index);
+}
+
+console.log("________________4______________");
+console.log("WAP to find first 10 Even numbers");
+
+for (var index = 2; index <=20; index= index+2) {
+   console.log(index);
+}
+
+
+
+console.log("________________5______________");
+console.log("WAP to print table of 5 upto 50");
+
+for (var index = 5; index <=50; index= index+5) {
+   console.log(index);
+}
+
+
+
+console.log("________________6______________");
+console.log("WAP to print table of 10 upto 100");
+
+for (var index = 10; index <=100; index= index+10) {
+   console.log(index);
+}
+
+
+console.log("________________7______________");
+console.log("WAP to print table of 10 in reverse order");
+
+for (var index = 100; index >=10; index= index-10) {
+   console.log(index);
+}
